ddc91e161d39eb23e788f799cc1092626d3eab7d
Eian Carter <eian@bricleir.com>
2020-08-27 21:29:18 -0700

    Added option to search by city state and other conditions invisible to client

--- src/app/pipes/searchfilter.pipe.ts
+++ src/app/pipes/searchfilter.pipe.ts
@@ -11,6 +11,8 @@ export class SearchFilterPipe implements PipeTransform {
             return items;
         }
         inputText = inputText.toLocaleLowerCase();
-        return items.filter((result: any) => result.name.toLocaleLowerCase().includes(inputText));
+        return items.filter((result: any) => {
+            return result.name.toLocaleLowerCase().includes(inputText) || result.brewery_type.toLocaleLowerCase().includes(inputText) || result.website_url.toLocaleLowerCase().includes(inputText) || result.city.toLocaleLowerCase().includes(inputText) || result.state.toLocaleLowerCase().includes(inputText)
+        });
     }
 }
